eb4878a6e2e42f7618d04c06d9d0853d2ab6a571
Jack Kill <jackkillschool@gmail.com>
2024-04-14 02:06:56 -0500

    Implemented extremely rudimentary search page
    No functionality, just added buttons and some very basic styling

--- 447project/src/components/pages/Search/Search.js
+++ 447project/src/components/pages/Search/Search.js
@@ -3,8 +3,21 @@ import './Search.css';
 
 const Search = () => {
   return (
-    <div className="page">
-        <p>Search</p>
+    <div className="search-page">
+        <div className="search-title">
+          <p>Search</p>
+        </div>
+        <div className="search-col-box">
+          <div className="search-desc">
+            <p>Select search parameters:</p>
+          </div>
+          <div className="search-button-box">
+            <button variant="outlined" className="search-button-member">Upcoming</button>
+            <button variant="outlined" className="search-button-member">Past</button>
+            <button variant="outlined" className="search-button-member">All</button>
+            <button variant="outlined" className="search-button-member">Specific Region</button>
+          </div>
+        </div>
     </div>
   );
 }
