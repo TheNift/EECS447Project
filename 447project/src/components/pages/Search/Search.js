20bfdc82f81fef5690763108f298b66091f43d5e
Jack <jackkillschool@gmail.com>
2024-04-24 12:58:44 -0500

    Trying to implement Jquery
    Need to push so I can switch systems because my laptop sucks :)

--- 447project/src/components/pages/Search/Search.js
+++ 447project/src/components/pages/Search/Search.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import $ from "jquery";
 import './Search.css';
 
+function useForceUpdate(){
+  const [value, setValue] = useState(0); // integer state
+  return () => setValue(value => value + 1); // update state to force render
+  // A function that increment 👆🏻 the previous state like here 
+  // is better than directly setting `setValue(value + 1)`
+}
+
 const Search = () => {
+  const [responseData, setResponseData] = useState(null);
+  const forceUpdate = useForceUpdate();
+
+  const handleClick = () => {
+    const url = "https://people.eecs.ku.edu/~j618p208/App.php";
+    const testData = "test=SENT DATA!!"; // Replace with your actual data
+
+    $.ajax({
+      url: url,
+      type: 'POST',
+      contentType: 'application/x-www-form-urlencoded',
+      data: testData,
+      success: function(data) {
+        setResponseData(data);
+        useForceUpdate();
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.error("Error:", textStatus, errorThrown);
+        // Handle errors appropriately
+      }
+    });
+  };
+
+
   return (
     <div className="search-page">
         <div className="search-title">
@@ -14,8 +46,11 @@ const Search = () => {
           <div className="search-button-box">
             <button variant="outlined" className="search-button-member">Upcoming</button>
             <button variant="outlined" className="search-button-member">Past</button>
-            <button variant="outlined" className="search-button-member">All</button>
+            <button variant="outlined" className="search-button-member" onClick={handleClick}>All</button>
             <button variant="outlined" className="search-button-member">Specific Region</button>
+          </div>
+          <div>
+            {responseData}
           </div>
         </div>
     </div>
